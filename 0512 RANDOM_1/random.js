9b991b4cd5a8b19dad37ab4c3981e441bbf95932
Marcin Napiorkowski <napiorkowski.marcin.gmail.com>
2020-09-03 12:59:22 +0200

    12 - PROBLEM TO SOLVE

---
+++ 0512 RANDOM_1/random.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector('button');
+const div = document.querySelector('div')
+div.textContent = 'lets draw'
+
+arr1 = [10, 11, 12, 13, 14, 15, 'a', 'b', 'c', 'd'];
+let i;
+let arrL = arr1.length;
+
+const arrGen = (arr1) => {
+    let i = Math.floor(Math.random() * arrL);
+    console.log(i);
+    //console.log(arr[i]);
+    div.textContent = `random element is ${arr1[i]};`
+};
+
+
+btn.addEventListener('click', arrGen);
